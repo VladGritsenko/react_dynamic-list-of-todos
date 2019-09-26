577cd9b97018c648180d7b9dfdccf7baca2cdfc8
Vlad Grytsenko <vladik.grytsenko2017@gmail.com>
2019-09-26 22:49:16 +0300

    Updates

---
+++ precache-manifest.be422d922bd770dc91b574f2a2870606.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa01f9c124fdad685ba47817ea3f854",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "dfea48b835cbcfedf89f",
+    "url": "/react_dynamic-list-of-todos/static/css/main.37d8d6a5.chunk.css"
+  },
+  {
+    "revision": "76e154d55f2799819537",
+    "url": "/react_dynamic-list-of-todos/static/js/2.43e87f33.chunk.js"
+  },
+  {
+    "revision": "dfea48b835cbcfedf89f",
+    "url": "/react_dynamic-list-of-todos/static/js/main.7ee9f0d9.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
